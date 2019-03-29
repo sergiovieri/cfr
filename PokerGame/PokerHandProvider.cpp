c28e19e7ed62ac5db84d1c5aaab1cdc590ac64fc
Sergio Vieri <sergio.vieri.hp@gmail.com>
2019-03-29 12:01:47 +0800

    PokerHandProvider: use ifstream.open

--- PokerGame/PokerHandProvider.cpp
+++ PokerGame/PokerHandProvider.cpp
@@ -1,12 +1,12 @@
 #include "PokerHandProvider.h"
 #include <cassert>
 
-ifstream PokerHandProvider::ifs = ifstream(PRECOM_FILE);
+ifstream PokerHandProvider::ifs;
 
 PokerHand PokerHandProvider::getPokerHand() {
     string s;
     ifs >> s;
-    if (s.empty()) ifs = ifstream(PRECOM_FILE);
+    if (s.empty()) ifs.open(PRECOM_FILE);
     ifs >> s;
     assert(s.size());
     return PokerHand(s);
