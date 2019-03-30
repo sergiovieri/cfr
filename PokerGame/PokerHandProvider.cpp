b6b5429ede397640e43b061b13e6aaad7b922a4c
Sergio Vieri <sergio.vieri.hp@gmail.com>
2019-03-30 09:31:55 +0800

    Fix ifstream reload

--- PokerGame/PokerHandProvider.cpp
+++ PokerGame/PokerHandProvider.cpp
@@ -1,12 +1,15 @@
 #include "PokerHandProvider.h"
 #include <cassert>
 
-ifstream PokerHandProvider::ifs;
+ifstream PokerHandProvider::ifs{PRECOM_FILE};
 
 PokerHand PokerHandProvider::getPokerHand() {
     string s;
     ifs >> s;
-    if (s.empty()) ifs.open(PRECOM_FILE);
+    if (s.empty()) {
+        ifs.clear();
+        ifs.seekg(0, ios::beg);
+    }
     ifs >> s;
     assert(s.size());
     return PokerHand(s);
